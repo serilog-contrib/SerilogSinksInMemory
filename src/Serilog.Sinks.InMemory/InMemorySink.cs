ba7305136514abc9e586ee3d781d5caff0c060d9
Sander van Vliet <sander@codenizer.nl>
2018-10-12 15:05:37 +0200

    Implement simple sink that captures log events for inspection

---
+++ src/Serilog.Sinks.InMemory/InMemorySink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Sinks.InMemory
+{
+    public class InMemorySink : ILogEventSink
+    {
+        private readonly List<LogEvent> _logEvents;
+
+        public InMemorySink()
+        {
+            _logEvents = new List<LogEvent>();
+        }
+
+        public IEnumerable<LogEvent> LogEvents => _logEvents.AsReadOnly();
+
+        public void Emit(LogEvent logEvent)
+        {
+            _logEvents.Add(logEvent);
+        }
+    }
+}
---
+++ test/Serilog.Sinks.InMemory.Tests.Unit/WhenLoggingToInMemorySink.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Serilog.Sinks.InMemory.Tests.Unit
+{
+    public class WhenLoggingToInMemorySink
+    {
+        [Fact]
+        public void GivenInformationMessageIsWritten_LogEventIsStoredInSink()
+        {
+            var sink = new InMemorySink();
+
+            var logger = new LoggerConfiguration()
+                .WriteTo.Sink(sink)
+                .CreateLogger();
+
+            logger.Information("Test");
+
+            sink
+                .LogEvents
+                .Should()
+                .HaveCount(1);
+        }
+    }
+}
