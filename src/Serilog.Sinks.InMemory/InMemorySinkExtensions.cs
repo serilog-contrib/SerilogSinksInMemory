f4cf2451f1b73c92f2c76892d8122e4845fb1d62
Sander van Vliet <sander@codenizer.nl>
2018-10-12 15:44:26 +0200

    Provide easier configuration method

---
+++ src/Serilog.Sinks.InMemory/InMemorySinkExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Serilog.Configuration;
+
+namespace Serilog.Sinks.InMemory
+{
+    public static class InMemorySinkExtensions
+    {
+        public static LoggerConfiguration InMemorySink(
+            this LoggerSinkConfiguration loggerConfiguration)
+        {
+            return loggerConfiguration.Sink(new InMemorySink());
+        }
+    }
+}
---
+++ test/Serilog.Sinks.InMemory.Tests.Unit/WhenConfiguringInMemorySink.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using FluentAssertions;
+using Serilog.Core;
+using Xunit;
+
+namespace Serilog.Sinks.InMemory.Tests.Unit
+{
+    public class WhenConfiguringInMemorySink
+    {
+        [Fact]
+        public void GivenConfigurationToWriteToInMemorySink_InMemorySinkIsAddedToLogger()
+        {
+            var logger = new LoggerConfiguration()
+                .WriteTo.InMemorySink()
+                .CreateLogger();
+
+            // Because there is no way to get access to the sinks configured for a logger
+            // we need to use reflection to get at it...
+
+            // The first one is a SafeAggregatedSink which is internal
+            var instance = logger
+                .GetType()
+                .GetField("_sink", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(logger);
+
+            // It has a field containing all configured sinks
+            var sinks = (ILogEventSink[])instance
+                .GetType()
+                .GetField("_sinks", BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(instance);
+
+            sinks.Should().Contain(s => s.GetType() == typeof(InMemorySink));
+        }
+    }
+}
