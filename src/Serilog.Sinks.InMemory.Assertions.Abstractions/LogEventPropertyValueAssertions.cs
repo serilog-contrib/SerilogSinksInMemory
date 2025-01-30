c446e47841d629ee171cac59eee97340a70989ef
Sander van Vliet <sander@codenizer.nl>
2025-01-30 09:45:33 +0100

    Remove commented out code

--- src/Serilog.Sinks.InMemory.Assertions.Abstractions/LogEventPropertyValueAssertions.cs
+++ src/Serilog.Sinks.InMemory.Assertions.Abstractions/LogEventPropertyValueAssertions.cs
@@ -5,7 +5,6 @@ namespace Serilog.Sinks.InMemory.Assertions;
 public interface LogEventPropertyValueAssertions
 {
     TValue WhichValue<TValue>();
-    //AndConstraint<LogEventAssertion> WithValue(object value, string because = "", params object[] becauseArgs);
     StructuredValueAssertions HavingADestructuredObject(string because = "", params object[] becauseArgs);
     AndConstraint<LogEventAssertion> WithValue(object value, string because = "", params object[] becauseArgs);
 }
