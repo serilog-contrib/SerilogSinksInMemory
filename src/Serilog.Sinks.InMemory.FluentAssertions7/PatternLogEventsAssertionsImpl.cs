400c1db3fb8e3cd57e7ba2cfb6b3d372038f21ef
Sander van Vliet <sander@codenizer.nl>
2025-01-30 10:26:07 +0100

    Add support for FluentAssertions 7

---
+++ src/Serilog.Sinks.InMemory.FluentAssertions7/PatternLogEventsAssertionsImpl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions.Execution;
+using Serilog.Events;
+using Serilog.Sinks.InMemory.Assertions;
+
+namespace Serilog.Sinks.InMemory.FluentAssertions7
+{
+    public class PatternLogEventsAssertionsImpl : LogEventsAssertionsImpl, PatternLogEventsAssertions
+    {
+        public PatternLogEventsAssertionsImpl(IEnumerable<LogEvent> subjectLogEvents) : base(null, subjectLogEvents)
+        {
+        }
+
+        public LogEventsAssertions Containing(
+            string pattern,
+            string because = "",
+            params object[] becauseArgs)
+        {
+            var matches = Subject
+                .Where(logEvent => logEvent.MessageTemplate.Text.Contains(pattern))
+                .ToList();
+
+            Execute.Assertion
+                .BecauseOf(because, becauseArgs)
+                .ForCondition(matches.Any())
+                .FailWith(
+                    "Expected a message with pattern {0} to be logged",
+                    pattern);
+
+            return new LogEventsAssertionsImpl(pattern, matches);
+        }
+    }
+}
