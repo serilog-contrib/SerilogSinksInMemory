6ea0c34a7ab8a86958df8499061007f0c5626bb6
Sander van Vliet <sander@codenizer.nl>
2025-02-10 16:03:23 +0100

    Add integration tests for Shouldly and AwesomeAssertions

---
+++ test/Serilog.Sinks.InMemory.Assertions.Tests.Integration.Shouldly4/WhenAssertingPropertyValuesOnMultipleMessages.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+using Serilog.Sinks.InMemory.Assertions;
+using Xunit.Sdk;
+
+namespace Serilog.Sinks.InMemory.Assertions.Tests.Unit
+{
+    public class WhenAssertingPropertyValuesOnMultipleMessages
+    {
+        [Fact]
+        public void GivenMessageDoesNotContainProperty_AssertionFails()
+        {
+            _logger.Information("Message without property");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message without property")
+                .Appearing().Times(1)
+                .WithProperty("SomeProperty");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected all instances of log message \"Message without property\" to have property \"SomeProperty\", but it was not found");
+        }
+
+        [Fact]
+        public void GivenOneMessageHasNullPropertyValue_AssertionFails()
+        {
+            _logger.Information("Message with {Property}", "val1");
+            _logger.Information("Message with {Property}", null);
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property}")
+                .Appearing().Times(2)
+                .WithProperty("Property")
+                .WithValues("val1", "val1");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected all instances of log message \"Message with {Property}\" to have property \"Property\", but it was not found");
+        }
+
+        [Fact]
+        public void GivenMessageAppearsTwiceWithUniquePropertyValues_AssertionForBothValuesPasses()
+        {
+            _logger.Information("Message with {Property}", "val1");
+            _logger.Information("Message with {Property}", "val2");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property}")
+                .Appearing().Times(2)
+                .WithProperty("Property")
+                .WithValues("val1", "val2");
+        }
+
+        [Fact]
+        public void GivenMessageAppearsTwiceAndAssertingOnThreeValues_AssertionFailsWithNumberOfValuesError()
+        {
+            _logger.Information("Message with {Property}", "val1");
+            _logger.Information("Message with {Property}", "val2");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property}")
+                .Appearing().Times(2)
+                .WithProperty("Property")
+                .WithValues("val1", "val2", "val3");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Can't assert property values because 3 values were provided while only 2 messages were expected");
+        }
+
+        [Fact]
+        public void GivenMessageAppearsTwiceWithSameValueAndAssertingOnOtherValue_AssertionFails()
+        {
+            _logger.Information("Message with {Property}", "val1");
+            _logger.Information("Message with {Property}", "val1");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property}")
+                .Appearing().Times(2)
+                .WithProperty("Property")
+                .WithValues("val1", "other");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected property values {\"val1\", \"val1\"} to contain {\"val1\", \"other\"} but did not find {\"other\"}");
+        }
+
+        [Fact]
+        public void AssertionShouldFailWithMissingValueEvenIfValuesAreInDifferentOrder()
+        {
+            _logger.Information("Message with {Property}", "val1");
+            _logger.Information("Message with {Property}", "val1");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property}")
+                .Appearing().Times(2)
+                .WithProperty("Property")
+                .WithValues("other", "val1");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected property values {\"val1\", \"val1\"} to contain {\"other\", \"val1\"} but did not find {\"other\"}");
+        }
+
+        [Fact]
+        public void GivenMessageWithTwoPropertiesAndAssertingOnBothProperies_AssertionPasses()
+        {
+            _logger.Information("Message with {Property1} and {Property2}", "val1", "valA");
+            _logger.Information("Message with {Property1} and {Property2}", "val2", "valB");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property1} and {Property2}")
+                .Appearing().Times(2)
+                .WithProperty("Property1")
+                .WithValues("val1", "val2")
+                .WithProperty("Property2")
+                .WithValues("valA", "valB");
+        }
+
+        [Fact]
+        public void GivenMessageWithTwoPropertiesAndAssertingOnBothProperiesWithOneValueNotPresentOnSecondProperty_AssertionFails()
+        {
+            _logger.Information("Message with {Property1} and {Property2}", "val1", "valA");
+            _logger.Information("Message with {Property1} and {Property2}", "val2", "XXX");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message with {Property1} and {Property2}")
+                .Appearing().Times(2)
+                .WithProperty("Property1")
+                .WithValues("val1", "val2")
+                .WithProperty("Property2")
+                .WithValues("valA", "valB");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected property values {\"valA\", \"XXX\"} to contain {\"valA\", \"valB\"} but did not find {\"valB\"}");
+        }
+
+        private readonly ILogger _logger;
+
+        public WhenAssertingPropertyValuesOnMultipleMessages()
+        {
+            _logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.InMemory()
+                .CreateLogger();
+        }
+    }
+}
---
+++ test/Serilog.Sinks.InMemory.Assertions.Tests.Integration.Shouldly4/WhenAssertingScalarLogPropertyExists.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Shouldly;
+using Serilog.Context;
+using Xunit;
+using Xunit.Sdk;
+using Serilog.Sinks.InMemory.Assertions;
+
+namespace Serilog.Sinks.InMemory.Assertions.Tests.Unit
+{
+    public class WhenAssertingScalarLogPropertyExists
+    {
+        private readonly ILogger _logger;
+
+        public WhenAssertingScalarLogPropertyExists()
+        {
+            _logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.InMemory()
+                .CreateLogger();
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithProperty_HavePropertySucceeds()
+        {
+            _logger.Information("Hello {name}", "World");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("Hello {name}")
+                .Appearing().Once()
+                .WithProperty("name");
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithoutProperty_HavePropertyFails()
+        {
+            _logger.Information("Hello {name}", "World");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Hello {name}")
+                .Appearing().Once()
+                .WithProperty("something else");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected message \"Hello {name}\" to have a property \"something else\" but it wasn't found");
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithPropertyAndAssertingValue_HavePropertySucceeds()
+        {
+            _logger.Information("Hello {name}", "World");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("Hello {name}")
+                .Appearing().Once()
+                .WithProperty("name")
+                .WithValue("World");
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithPropertyAndAssertingDifferentValue_HavePropertyFails()
+        {
+            _logger.Information("Hello {name}", "World");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Hello {name}")
+                .Appearing().Once()
+                .WithProperty("name")
+                .WithValue("BLABLABLA");
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected property \"name\" to have value \"BLABLABLA\" but found \"World\"");
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithIntegerPropertyValue_HavePropertySucceeds()
+        {
+            _logger.Information("Message number {number}", 5);
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message number {number}")
+                .Appearing().Once()
+                .WithProperty("number")
+                .WithValue(5);
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithIntegerPropertyAndAssertingDifferentValue_HavePropertyFails()
+        {
+            _logger.Information("Message number {number}", 5);
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message number {number}")
+                .Appearing().Once()
+                .WithProperty("number")
+                .WithValue(2);
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe("Expected property \"number\" to have value 2 but found 5");
+        }
+
+        [Fact]
+        public void GivenMessageIsLoggedWithPropertyFromContext_HavePropertySucceeds()
+        {
+            using (LogContext.PushProperty("some_property", "some_value"))
+            {
+                _logger.Information("Message number {number}", 5);
+            }
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage("Message number {number}")
+                .Appearing().Once()
+                .WithProperty("some_property")
+                .WithValue("some_value");
+        }
+
+        [Fact]
+        public void GivenLogMessageWithTwoProperties_BothPropertiesExistOnLogEntry()
+        {
+            _logger.Information("{PropertyOne} {PropertyTwo}", "one", "two");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage()
+                .Appearing().Once()
+                .WithProperty("PropertyOne")
+                .WithValue("one")
+                .WithProperty("PropertyTwo")
+                .WithValue("two");
+        }
+
+        [Fact]
+        public void GivenLogMessageWithIntegerProperty_CanAssertUsingFluentAssertions()
+        {
+            _logger.Information("{PropertyOne}", 5);
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage()
+                .Appearing().Once()
+                .WithProperty("PropertyOne")
+                .WhichValue<int>()
+                .ShouldBeLessThanOrEqualTo(10);
+        }
+
+        [Fact]
+        public void GivenLogMessageWithStringProperty_CanAssertUsingFluentAssertions()
+        {
+            _logger.Information("{PropertyOne}", "bar");
+
+            InMemorySink.Instance
+                .Should()
+                .HaveMessage()
+                .Appearing().Once()
+                .WithProperty("PropertyOne")
+                .WhichValue<string>()
+                .Length
+                .ShouldBe(3);
+        }
+
+        [Fact]
+        public void GivenLogMessageWithStringPropertyAndAssertingOnInt_AssertionFailsBecauseOfTypeMismatch()
+        {
+            _logger.Information("{PropertyOne}", "bar");
+
+            Action action = () => InMemorySink.Instance
+                .Should()
+                .HaveMessage()
+                .Appearing().Once()
+                .WithProperty("PropertyOne")
+                .WhichValue<int>()
+                .ShouldBeLessThan(3);
+
+            Should.Throw<Exception>(() => action())
+                .Message
+                .ShouldBe($"Expected property value to be of type \"{nameof(Int32)}\" but found \"{nameof(String)}\"");
+        }
+    }
+}
